c05da68fac8c7b2c56967ceea0d7eed837b31f53
mertbabspro <mertmertmert416@gmail.com>
2025-08-10 00:08:20 +0300

    Fix requirements.txt

--- fiyat.py
+++ fiyat.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import json
 import requests
